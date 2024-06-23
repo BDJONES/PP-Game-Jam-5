a19e0fe7c3aa306a1721f9fef89e9fc3a262d0cd
Brandon Jones <72243416+BDJONES@users.noreply.github.com>
2024-06-23 14:57:56 -0400

    Update meteor.gd

--- Ship Repair/Scripts/meteor.gd
+++ Ship Repair/Scripts/meteor.gd
@@ -4,7 +4,7 @@ const SPEED = 60
 
 # Probably need the ship here to update the ships health on collisions 
 @onready var ship_script = get_node("../Ship/Area2D").get_script()
-
+signal damaged_ship
 
 func _physics_process(delta):
 	position.y += SPEED * delta
@@ -15,7 +15,8 @@ func _on_body_entered(body):
 	# not take a number unless we want to specify how much damage 
 	# we want the ship to take
 	# ship.descrease_health(10)
-	ship_script.decrease_health(10)
+	ship_script.decrease_health(25)
+	damaged_ship.emit()
 	queue_free()
 
 func meteor():
